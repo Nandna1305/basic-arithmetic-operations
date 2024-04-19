91d445a66c570b9baac2bf79058332ba693b737f
Nandna1305 <167091623+Nandna1305@users.noreply.github.com>
2024-04-19 15:01:44 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,9 +2,12 @@ import java.lang.System;
 import java.util.Scanner;
 class hello{
     public static void main(String args[]){
+//to create a object using class scanner
         Scanner nandana = new Scanner(System.in);
+//for storing values/assigning values
         int a = nandana.nextInt();
         int b = nandana.nextInt();
+ //for printing the output
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
