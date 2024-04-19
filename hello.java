054faf8a0cc8de43a1f73e1702c957fe9270742d
unknown <tommyandjerry1330@gmail.com>
2024-04-19 14:59:03 +0530

    added file hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String args[]){
+        Scanner nandana = new Scanner(System.in);
+        int a = nandana.nextInt();
+        int b = nandana.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
